a7251c58b226ecf38553df4799e995fab7a2bf62
wangyishuo <2227172350@qq.com>
2025-11-24 22:13:51 +0800

    2025.11.24

--- leetcode/src/main/java/everyDay/Code.java
+++ leetcode/src/main/java/everyDay/Code.java
@@ -1,12 +1,10 @@
 package everyDay;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Code {
     public static void main(String[] args) {
-        int[][] nums = new int[][]{{1,3},{3,7},{8,9}};
+        int[][] nums = new int[][]{{1,3},{1,4},{2,5},{3,5}};
         intersectionSizeTwo(nums);
     }
     public static int minOperations(int[] nums) {
@@ -81,33 +79,64 @@ public class Code {
     //leetcode757，先按照左侧排序，然后贪心取就可以了
     public static int intersectionSizeTwo(int[][] intervals) {
         int n = intervals.length;
-        int res = 0;
-        int m = 2;
-        Arrays.sort(intervals, (a, b) -> {
-            if (a[0] == b[0]) {
-                return b[1] - a[1];
+        Arrays.sort(intervals,new Comparator<int[]>(){
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] == o2[0]?o1[1]-o2[1]:o1[0]-o2[0];
             }
-            return a[0] - b[0];
         });
-        List<Integer>[] temp = new List[n];
-        for (int i = 0; i < n; i++) {
-            temp[i] = new ArrayList<Integer>();
+        int leftmax = intervals[n-1][0];
+        int leftmin = intervals[0][0];
+        int rightmax = Integer.MIN_VALUE;
+        int rightmin = Integer.MAX_VALUE;
+        for(int[] nums:intervals){
+            rightmax = Math.max(rightmax,nums[1]);
+            rightmin = Math.min(rightmin,nums[1]);
         }
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = intervals[i][0], k = temp[i].size(); k < m; j++, k++) {
-                res++;
-                help(intervals, temp, i - 1, j);
-            }
+        if(leftmax<=rightmin){
+            return rightmin+1-(leftmax-1)+1;
+        }else{
+            return (Integer.min(rightmin,rightmax)+1)-(rightmin+1);
         }
-        return res;
+//        int n = intervals.length;
+//        int res = 0;
+//        int m = 2;
+//        Arrays.sort(intervals, (a, b) -> {
+//            if (a[0] == b[0]) {
+//                return b[1] - a[1];
+//            }
+//            return a[0] - b[0];
+//        });
+//        List<Integer>[] temp = new List[n];
+//        for (int i = 0; i < n; i++) {
+//            temp[i] = new ArrayList<Integer>();
+//        }
+//        for (int i = n - 1; i >= 0; i--) {
+//            for (int j = intervals[i][0], k = temp[i].size(); k < m; j++, k++) {
+//                res++;
+//                help(intervals, temp, i - 1, j);
+//            }
+//        }
+//        return res;
     }
 
-    public static void help(int[][] intervals, List<Integer>[] temp, int pos, int num) {
-        for (int i = pos; i >= 0; i--) {
-            if (intervals[i][1] < num) {
-                break;
-            }
-            temp[i].add(num);
+//    public static void help(int[][] intervals, List<Integer>[] temp, int pos, int num) {
+//        for (int i = pos; i >= 0; i--) {
+//            if (intervals[i][1] < num) {
+//                break;
+//            }
+//            temp[i].add(num);
+//        }
+//    }
+    //leetcode1018，思路：位运算符<<
+    public static List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> ans = new ArrayList<>(nums.length); // 预分配空间
+        int x = 0;
+        for (int bit : nums) {
+            x = (x << 1 | bit) % 5;
+            ans.add(x == 0);
         }
+        return ans;
     }
+
 }
